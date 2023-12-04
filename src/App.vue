717ce3da553aaf224f1b4c5a1b4114fa1e0244b5
zhang san <1716969013@qq.com>
2023-12-04 19:42:19 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <!-- 路由的出口 -->
+    <router-view/>
+    <h1>黑马头条</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+      name:'App'
+  }
+</script>
+
+
+<style lang="less">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+//路由表
+const routes = [
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
